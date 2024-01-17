73b061ab32202543a3fb9a4fd236e0bec77dfb7b
Wiktor <wiktor.rostkowski@hotmail.com>
2024-01-17 16:17:45 +0100

    Update main.py
    Add changing directions, required points Start Stop

--- lab3 SimpleGame/main.py
+++ lab3 SimpleGame/main.py
@@ -1,4 +1,5 @@
 #Wiktor Rostkowski s23141
+
 import random
 
 
@@ -10,9 +11,8 @@ class SimpleGame:
         self.start = (random.randint(0, width - 1), random.randint(0, height - 1))
         self.stop = self.generate_stop()
         self.obstacles = self.generate_obstacles()
+        self.path = []
         self.generate_path()
-
-        self.display_board()
 
     def generate_stop(self):
         stop = (random.randint(0, self.width - 1), random.randint(0, self.height - 1))
@@ -34,9 +34,31 @@ class SimpleGame:
     def generate_path(self):
         x, y = self.start
 
+        for obstacle in self.obstacles:
+            self.board[obstacle[1]][obstacle[0]] = 'X'
+
+        self.board[y][x] = '*'
+        self.path.append((x, y))
+
+
     def display_board(self):
         for row in self.board:
-            print('-'.join(row))
+            print(' '.join(row))
+        print(f"Start: {self.start}")
+        print(f"Stop: {self.stop}")
+        print(f"Obstacles: {self.obstacles}")
+        print(f"Path: {self.path}")
+
+
+    def save_to_file(self, filename='board.txt'):
+        with open(filename, 'w') as file:
+            for row in self.board:
+                file.write(' '.join(row) + '\n')
+            file.write(f"Start: {self.start}\n")
+            file.write(f"Stop: {self.stop}\n")
+            file.write(f"Obstacles: {self.obstacles}\n")
+            file.write(f"Path: {self.path}\n")
+
 
     def move(self, direction):
         x, y = self.start
@@ -50,13 +72,26 @@ class SimpleGame:
             self.start = (x, y + 1)
 
 
+# Start Game
 game = SimpleGame(5, 5)
 
-# Test ruchu
-game.move('right')
-game.display_board()
+
+while True:
+    game.display_board()
+    direction = input("Podaj kierunek ruchu (left/right/up/down): ")
+    if direction.lower() in ['left', 'right', 'up', 'down']:
+        game.move(direction.lower())
+
+        # Dodaj nową pozycję do ścieżki i oznacz na planszy
+        game.path.append(game.start)
+        game.board[game.start[1]][game.start[0]] = '*'
+
+        # Sprawdź, czy osiągnięto cel
+        if game.start == game.stop:
+            print("Gratulacje! Osiągnięto cel.")
+            break
+    else:
+        print("Nieprawidłowy kierunek. Podaj 'left', 'right', 'up' lub 'down'.")
 
 # Zapis planszy do pliku
-with open('board.txt', 'w') as file:
-    for row in game.board:
-        file.write(' '.join(row) + '\n')
+game.save_to_file('board.txt')
